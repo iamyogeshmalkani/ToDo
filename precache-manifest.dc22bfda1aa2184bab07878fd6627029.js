80ca61b9aebb3a6c0cfbd8857efd6c29098335d4
Yogesh Malkani <conan.yogesh@gmail.com>
2021-10-11 21:56:34 +0530

    Updates

---
+++ precache-manifest.dc22bfda1aa2184bab07878fd6627029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa630b9d9525175d6e4fa6281a5bcdb2",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "0d64afdd792e355e48e0",
+    "url": "/ToDo/static/css/main.41b1c429.chunk.css"
+  },
+  {
+    "revision": "91a73cf732faa3eccf73",
+    "url": "/ToDo/static/js/2.50f03044.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ToDo/static/js/2.50f03044.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d64afdd792e355e48e0",
+    "url": "/ToDo/static/js/main.de0a6cf8.chunk.js"
+  },
+  {
+    "revision": "e601b6347bf59fcd61db",
+    "url": "/ToDo/static/js/runtime-main.0ec23361.js"
+  }
+]);
